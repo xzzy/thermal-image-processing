9e5ff80db9a6fab5b262b4f53f2f71459475c544
shibaken <katsufumi.shibata@dbca.wa.gov.au>
2025-11-24 15:41:57 +0800

    Use absolute path for log file in thermal processing script

--- thermalimageprocessing/thermal_image_processing.sh
+++ thermalimageprocessing/thermal_image_processing.sh
@@ -3,25 +3,29 @@ folder_path=`pwd`
 echo $folder_path
 full_filename=$1
 uploads_folder_path=$2
+
+mkdir -p "$folder_path/logs"
+LOG_FILE="$folder_path/logs/log.txt"
+
 if [ -z "$full_filename" ]; then
     echo "No filename provided"
-    printf "\nNo filename provided\n" >> ./logs/log.txt
+    printf "\nNo filename provided\n" >> "$LOG_FILE"
     exit 1
 fi
 
 if [ -z "$uploads_folder_path" ]; then
     echo "No uploads folder path provided"
-    printf "\nNo uploads folder path provided\n" >> ./logs/log.txt
+    printf "\nNo uploads folder path provided\n" >> "$LOG_FILE"
     exit 1
 fi
 filename="${full_filename%.7z*}.7z"
 basename_filename=`basename $filename`
 timestamp=$(date +"%Y-%m-%d_%H:%M:%S")
-printf "\n%s filename: %s" "$timestamp" "$filename" >> ./logs/log.txt
+printf "\n%s filename: %s" "$timestamp" "$filename" >> "$LOG_FILE"
 #if [[ $filename == *.7z ]]; then
 
 dirname="${basename_filename%.*}"
-printf "\ndirname: $dirname" >> ./logs/log.txt
+printf "\ndirname: $dirname" >> "$LOG_FILE"
 sleep 10
 echo "STARTING"
 echo $filename
@@ -43,4 +47,4 @@ echo "Run pythonscript"
 python $folder_path/thermalimageprocessing/thermal_image_processing.py  $folder_path/thermal_data_processing/$dirname
 
 end_timestamp=$(date +"%Y-%m-%d_%H:%M:%S")
-printf "\n%s %s done" $end_timestamp $dirname  >> ./logs/log.txt
+printf "\n%s %s done" "$end_timestamp" "$dirname"  >> "$LOG_FILE"
